57acd799e632556627d9e522ee5dfe2847799ebd
Danielle Smith <57499759+Danielle-Smith@users.noreply.github.com>
2020-03-03 09:47:51 -0700

    Add files via upload

---
+++ Ternary.js
@@ -0,0 +1,12 @@
+function ageVerification(age) {
+    // if (age > 25) {
+    //   console.log('can rent a car');
+    // } else {
+    //   console.log("can't rent a car");
+    // }
+  
+    return age > 25 ? "can rent a car" : "can't rent a car"; // same as above conditional but with one line of code.
+  }
+  
+  ageVerification(30); 
+  ageVerification(10); 
